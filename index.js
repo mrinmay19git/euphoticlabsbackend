67c1afec2ba017979d1687cf88be985260efd6ce
mrinmay19git <saymrinmay19@gmail.com>
2024-07-16 03:45:38 +0530

    firs commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
+const cors =require("cors");
+
 
 
 const databasePath = path.join(__dirname, "newdata.db");
@@ -9,6 +11,7 @@ const databasePath = path.join(__dirname, "newdata.db");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 let database = null;
 
